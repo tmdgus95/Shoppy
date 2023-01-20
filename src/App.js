ff6d550943070ffc5761575588fd4a3ca64ec86b
tmdgus95 <8571053@naver.com>
2023-01-20 23:23:37 +0900

    헤더 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
 function App() {
-    return <div></div>;
+    return (
+        <>
+            <Navbar />
+            <Outlet />
+        </>
+    );
 }
 
 export default App;
