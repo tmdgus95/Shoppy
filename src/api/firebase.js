e1b1251db71f9d7b593fc75db03820214cdd1294
tmdgus95 <8571053@naver.com>
2023-01-21 12:51:58 +0900

    로그인 유저 정보 아바타

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -17,23 +17,16 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
-
 provider.setCustomParameters({
     prompt: 'select_account',
 });
 
-export async function login() {
-    return signInWithPopup(auth, provider)
-        .then((result) => {
-            const user = result.user;
-            console.log(user);
-            return user;
-        })
-        .catch(console.error);
+export function login() {
+    signInWithPopup(auth, provider).catch(console.error);
 }
 
-export async function logout() {
-    return signOut(auth).then(() => null);
+export function logout() {
+    signOut(auth).then(() => null);
 }
 
 export function onUserStateChange(callback) {
