a8b010ad634b98640b93a0d75ffa3bf317007899
tmdgus95 <8571053@naver.com>
2023-01-21 17:35:47 +0900

    경로 보호 및 어드민 사용자 식별기능

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import AllProducts from './pages/AllProducts';
 import NewProduct from './pages/NewProduct';
 import ProductDetail from './pages/ProductDetail';
 import MyCart from './pages/MyCart';
+import ProtectedRoute from './pages/ProtectedRoute';
 
 const router = createBrowserRouter([
     {
@@ -22,9 +23,23 @@ const router = createBrowserRouter([
                 element: <Home />,
             },
             { path: '/products', element: <AllProducts /> },
-            { path: '/products/new', element: <NewProduct /> },
+            {
+                path: '/products/new',
+                element: (
+                    <ProtectedRoute requireAdmin>
+                        <NewProduct />
+                    </ProtectedRoute>
+                ),
+            },
             { path: '/products/:id', element: <ProductDetail /> },
-            { path: '/carts', element: <MyCart /> },
+            {
+                path: '/carts',
+                element: (
+                    <ProtectedRoute>
+                        <MyCart />
+                    </ProtectedRoute>
+                ),
+            },
         ],
     },
 ]);
---
+++ src/pages/ProtectedRoute.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuthContext } from '../components/context/AuthContext';
+
+export default function ProtectedRoute({ children, requireAdmin }) {
+    const { user } = useAuthContext();
+    if (!user || (requireAdmin && !user.idAdmin)) {
+        return <Navigate to='/' replace />;
+    }
+    return children;
+}
