bc2f5059ee29a967ee44402eca419d50c5861b49
tmdgus95 <8571053@naver.com>
2023-01-21 22:02:19 +0900

    파이어 베이스 연동

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
+import { addNewProduct } from '../api/firebase';
 import { uploadImage } from '../api/uploader';
 import Button from '../components/ui/Button';
 
@@ -9,7 +10,7 @@ export default function NewProduct() {
     const handleSubmit = (e) => {
         e.preventDefault();
         uploadImage(file).then((url) => {
-            console.log(url);
+            addNewProduct(product, url);
         });
     };
     const handleChange = (e) => {
