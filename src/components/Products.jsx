f3d62c63dec2257f01dacb5bbaf02341669ee357
tmdgus95 <8571053@naver.com>
2023-01-22 22:25:16 +0900

    제품 추가 리팩토링

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,10 +1,11 @@
-import { useQuery } from '@tanstack/react-query';
 import React from 'react';
-import { getProducts } from '../api/firebase';
+import useProducts from '../hooks/useProduts';
 import ProductCard from './ProductCard';
 
 export default function Products() {
-    const { isLoading, error, data: products } = useQuery(['products'], getProducts);
+    const {
+        productsQuery: { isLoading, error, data: products },
+    } = useProducts();
     return (
         <>
             {isLoading && <p>Loading...</p>}
--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -1,25 +1,32 @@
 import React from 'react';
 import { useState } from 'react';
-import { addNewProduct } from '../api/firebase';
 import { uploadImage } from '../api/uploader';
 import Button from '../components/ui/Button';
+import useProducts from '../hooks/useProduts';
 
 export default function NewProduct() {
     const [product, setProduct] = useState({});
     const [file, setFile] = useState();
     const [isUploading, setIsUploading] = useState(false);
     const [success, setSuccess] = useState(false);
+    const { addProduct } = useProducts();
+
     const handleSubmit = (e) => {
         e.preventDefault();
         setIsUploading(true);
         uploadImage(file) //
             .then((url) => {
-                addNewProduct(product, url).then(() => {
-                    setSuccess('성공적으로 제품이 추가되었습니다.');
-                    setTimeout(() => {
-                        setSuccess(null);
-                    }, 4000);
-                });
+                addProduct.mutate(
+                    { product, url },
+                    {
+                        onSuccess: () => {
+                            setSuccess('성공적으로 제품이 추가되었습니다.');
+                            setTimeout(() => {
+                                setSuccess(null);
+                            }, 4000);
+                        },
+                    }
+                );
             })
             .finally(() => setIsUploading(false));
     };
